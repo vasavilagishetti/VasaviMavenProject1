95b258c2d05cef75abc71b7b4538951b0b8e610f
vasavi <vasavi678@gmail.com>
2021-02-20 23:23:45 +0000

    New class is added

--- src/test/java/demoTest2.java
+++ src/test/java/demoTest2.java
@@ -2,4 +2,7 @@ public class demoTest2 {
     public void testing(){
         System.out.println("new test class is created");
     }
+    public void testing2(){
+        System.out.println("one more test added");
+    }
 }
