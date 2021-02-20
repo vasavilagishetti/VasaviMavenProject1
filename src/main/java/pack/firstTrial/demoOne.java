90737b2fdcec506fec3e2db0afd3bb7b6bcac2ef
vasavi <vasavi678@gmail.com>
2021-02-20 21:38:51 +0000

    commiting the files to local Repo.

---
+++ src/main/java/pack/firstTrial/demoOne.java
@@ -0,0 +1,18 @@
+package pack.firstTrial;
+
+public class demoOne {
+    int a = 2;
+    int b = 4;
+
+    public void something(){
+        int c = a+b;
+        //System.out.println(c);
+        }
+
+    /*public static void main(String[] args) {
+        demoOne code = new demoOne();
+        code.something();
+        }
+     */
+
+  }
---
+++ src/test/java/demoTest1.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+import pack.firstTrial.demoOne;
+import org.junit.Test;
+
+public class demoTest1 {
+     @Test
+     public void Test1(){
+           System.out.println("print test1");
+
+     }
+     @Test
+     public void Test2(){
+          demoOne test = new demoOne();
+          test.something();
+     }
+
+
+}
+
